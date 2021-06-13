baa70ff5d6e41537c18e0699d974b2d51bacf179
yait-ucess <bandai28@ezweb.ne.jp>
2021-06-13 13:59:06 +0900

    Global Stateの追加
    1.Data Storeの作成（initialState, reducer, contextAPI(createContext)）
    2.Context ProviderでComponentを囲う（Consumer Component）
    3.参照 ← Stateの呼び出しにはuseContextを使用
    4.更新 ← Storeの更新にはDispatch関数を使用

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Layout from '../components/layout/Layout';
 import { fetchPopularData } from '../apis/index';
+import { Store } from '../store/index';
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store);
   useEffect(() => {
     fetchPopularData().then((res) => {
-      console.log('data', res)
+      console.log('data', res);
+      setGlobalState({type: 'SET_POPULAR', payload: {popular: res.data.items}})
     })
+    //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
     <Layout>
