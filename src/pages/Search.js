541e2842c8825c338cef5385237b76991483643e
yait-ucess <bandai28@ezweb.ne.jp>
2021-06-16 11:40:14 +0900

    検索結果の取得と表示
    reducer, useContext, state, storeを使って実装。
    検索を複数回連続でしても対応可能。

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Layout from '../components/layout/Layout';
+import { useLocation } from 'react-router-dom';
+import { fetchSearchData } from '../apis/index';
+import { Store } from '../store/index';
+import VideoGrid from '../components/videoGrid/VideoGrid';
+import VideoGridItem from '../components/videoGridItem/VideoGridItem';
 
 const Search = () => {
+  const { globalState, setGlobalState } = useContext(Store)
+  const location = useLocation();
+  const setSearchResult = async () => {
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get('query');
+    if (query) {
+      await fetchSearchData(query).then((res) => {
+        setGlobalState({ type: 'SET_SEARCHED', payload: { searched: res.data.items } });
+      })
+    }
+  }
+  useEffect(() => {
+    setSearchResult();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.search])
   return (
     <Layout>
-      Search page
+      <VideoGrid>
+        {
+          globalState.searched ? globalState.searched.map((search) => {
+            return (
+              <VideoGridItem
+                id={search.id.videoId}
+                key={search.id.videoId}
+                src={search.snippet.thumbnails.medium.url}
+                title={search.snippet.title}
+              />
+            )
+          }) : <span>No data</span>
+        }
+      </VideoGrid> 
     </Layout>
   )
 }
